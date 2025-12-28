ff549c80797d78014fff474c7d87d6fe2eb73d2d
Saiful Islam <saifulislamsany44@gmail.com>
2025-12-28 15:08:24 +0600

    personal id mint3

--- minikit.config.ts
+++ minikit.config.ts
@@ -81,11 +81,11 @@ const ROOT_URL = "https://mint.personalids.xyz";
 
 
 export const minikitConfig: MiniAppManifest = {
-  accountAssociation: {
-    header: "eyJmaWQiOjEwNTAyNjgsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHhkM2U4MzhFQThmMjczYzdkMzlDNmViMzUxMDMwOGJGNUNkYUUyY2VFIn0",
-    payload: "eyJkb21haW4iOiJtaW50LnBlcnNvbmFsaWRzLnh5eiJ9",
-    signature: "sZBMFygdlbt5QDf2mGFW984Hek0qfWIOwjeYltVD3HogwvctWdu5IubQNZDf0A0Vinra0QJk1tCcHJb5ZJqrSxs=",
-  },
+  // accountAssociation: {
+  //   header: "eyJmaWQiOjEwNTAyNjgsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHhkM2U4MzhFQThmMjczYzdkMzlDNmViMzUxMDMwOGJGNUNkYUUyY2VFIn0",
+  //   payload: "eyJkb21haW4iOiJtaW50LnBlcnNvbmFsaWRzLnh5eiJ9",
+  //   signature: "sZBMFygdlbt5QDf2mGFW984Hek0qfWIOwjeYltVD3HogwvctWdu5IubQNZDf0A0Vinra0QJk1tCcHJb5ZJqrSxs=",
+  // },
 
 
   miniapp: {
