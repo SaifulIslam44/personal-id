b0ac7a4a3e0c308a51e1af5035caa38fe8bd9cdd
Saiful Islam <saifulislamsany44@gmail.com>
2026-04-08 17:33:19 +0600

    personal id mint airdrop release74

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,40 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   webpack: (config) => {
+//     config.externals.push("pino-pretty", "lokijs", "encoding");
+//     return config;
+//   },
+// };
+
+// export default nextConfig;
+
+
+
+
+
+
+
+
+
+
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // 🔴 Image Optimization বন্ধ করা এবং ডোমেইন পারমিশন দেওয়া
+  images: {
+    unoptimized: true, 
+    remotePatterns: [
+      { protocol: 'https', hostname: 'imagedelivery.net' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: 'i.pravatar.cc' },
+      { protocol: 'https', hostname: 'placehold.co' },
+    ],
+  },
+
+  // আপনার আগের Webpack কনফিগারেশন
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
